3b317e3e3b0200ed92c67eb79e02c7bafd26b941
Rifqi Akhmad <rifki.ahmad2003@gmail.com>
2023-12-09 18:11:29 +0700

    feat: checkpoint1

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import crypto, { randomUUID } from 'crypto';
 const app = express();
 const port = 3000;
 
-function encrypt(input, key) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', key);
+function encrypt(input, key, iv) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
     let encrypted = cipher.update(input, 'utf-8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
 }
   
   // Function to decrypt a string
-function decrypt(input, key) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key);
+function decrypt(input, key, iv) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     let decrypted = decipher.update(input, 'hex', 'utf-8');
     decrypted += decipher.final('utf-8');
     return decrypted;
@@ -30,18 +30,23 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
     const newFileName = randomUUID().toString()
     
+    const len = req.body.seed.length
+
+    const iv = crypto.randomBytes(len)
+    console.log(iv, req.body.seed)
+
     if (fileExtension === '.jpg' || fileExtension === '.jpeg' || fileExtension === '.png') {
         const imageData = fs.readFileSync(req.file.path);
         const exifData = px.load(imageData.toString('binary'));
         
-        exifData['GPS'][px.GPSIFD.GPSLatitude] = encrypt(exifData['GPS'][px.GPSIFD.GPSLatitude], req.body.seed).toString()
-        exifData['GPS'][px.GPSIFD.GPSLongitude] = encrypt(exifData['GPS'][px.GPSIFD.GPSLongitude], req.body.seed).toString()
-        exifData['GPS'][px.GPSIFD.GPSSatellites] = encrypt(exifData['GPS'][px.GPSIFD.GPSSatellites], req.body.seed).toString()
-        exifData['0th']['306'] = encrypt(exifData['0th']['306'], req.body.seed).toString()
-        exifData.Exif['36867'] = encrypt(exifData.Exif['36867'], req.body.seed).toString()
-        exifData.Exif['36868'] = encrypt(exifData.Exif['36868'], req.body.seed).toString()
-        exifData['0th']['271'] = encrypt(exifData['0th']['271'], req.body.seed).toString()
-        exifData['0th']['272'] = encrypt(exifData['0th']['272'], req.body.seed).toString()
+        exifData['GPS'][px.GPSIFD.GPSLatitude] = encrypt(exifData['GPS'][px.GPSIFD.GPSLatitude], req.body.seed, iv)
+        exifData['GPS'][px.GPSIFD.GPSLongitude] = encrypt(exifData['GPS'][px.GPSIFD.GPSLongitude], req.body.seed, iv)
+        exifData['GPS'][px.GPSIFD.GPSSatellites] = encrypt(exifData['GPS'][px.GPSIFD.GPSSatellites], req.body.seed, iv)
+        exifData['0th']['306'] = encrypt(exifData['0th']['306'], req.body.seed, iv)
+        exifData.Exif['36867'] = encrypt(exifData.Exif['36867'], req.body.seed, iv)
+        exifData.Exif['36868'] = encrypt(exifData.Exif['36868'], req.body.seed, iv)
+        exifData['0th']['271'] = encrypt(exifData['0th']['271'], req.body.seed, iv)
+        exifData['0th']['272'] = encrypt(exifData['0th']['272'], req.body.seed, iv)
     
         const editedImageData = px.insert(px.dump(exifData), imageData.toString('binary'));
         fs.writeFileSync(req.file.path, editedImageData, 'binary');
@@ -54,7 +59,8 @@ app.post('/upload', upload.single('file'), (req, res) => {
 
     res.json({ message: 'File uploaded successfully',
                 data: {
-                    link: fileLink
+                    link: fileLink,
+                    recovery_cipher: iv
                 }
     });
 });
